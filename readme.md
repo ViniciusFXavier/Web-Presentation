d8a57166f2bd036ca635f39c95a3b6608d344aa6
Vinícius Francisco Xavier <viniciusfranciscoxavier@gmail.com>
2019-11-10 19:09:18 -0300

    Arquivos

---
+++ readme.md
@@ -0,0 +1,10 @@
+# WEB
+
+### Pré-requisito
+```
+npm install http-server -g
+```
+
+### Web Worker
+Na pasta do arquivo execute `http-server`
+
---
+++ shared-worker/chat/worker.js
@@ -0,0 +1,33 @@
+var nextName = 0;
+function getNextName() {
+  return nextName++;
+}
+
+var viewers = {};
+onconnect = function (event) {
+  var name = getNextName();
+  event.ports[0]._data = { port: event.ports[0], name: name, x: 0, y: 0, };
+  viewers[name] = event.ports[0]._data;
+  event.ports[0].postMessage('cfg ' + name);
+  event.ports[0].onmessage = getMessage;
+};
+
+function getMessage(event) {
+  switch (event.data.substr(0, 4)) {
+    case 'txt ':
+      var name = event.target._data.name;
+      var message = event.data.substr(4);
+      for (var viewer in viewers)
+        viewers[viewer].port.postMessage('txt ' + name + ' ' + message);
+      break;
+    case 'msg ':
+      var party1 = event.target._data;
+      var party2 = viewers[event.data.substr(4).split(' ', 1)[0]];
+      if (party2) {
+        var channel = new MessageChannel();
+        party1.port.postMessage('msg ' + party2.name, [channel.port1]);
+        party2.port.postMessage('msg ' + party1.name, [channel.port2]);
+      }
+      break;
+  }
+}
